2ee4693d20c06a577428702c4d01d04d1f45e2b2
cristian <cristian.fonolla@gmail.com>
2016-10-13 21:59:43 +0200

    creacio de templatecordova

---
+++ resources/assets/js/app.js
@@ -0,0 +1,12 @@
+//Opció 1 require
+//Opció 2 import: CUIDAO QUIETORLLLLLLLLLL els dos van en navegador normals
+
+window.$ = window.jQuery = require ('jquery');
+window._ = require('underscore');
+require('bootstrap-sass');
+
+
+
+$(document).ready(function(){
+    $("#msgid").html("Hello world");
+});
